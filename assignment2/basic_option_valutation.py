90f21bd17415c5ccb21c2596442fb074aa859978
mbruin <menno.bruin@student.uva.nl>
2021-02-25 17:56:20 +0100

    implemented value calculation

--- assignment2/basic_option_valutation.py
+++ assignment2/basic_option_valutation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 t = 1
 dt = 1 / 365
@@ -23,5 +24,27 @@ def euler_option_valuation(plot=False):
         plt.plot(range(n), stock_prices, 'r-')
         plt.show()
 
+    return stock_prices[-1]
 
-euler_option_valuation(plot=True)
+
+option_values = []
+sample_paths = 10 ** np.arange(2, 7, 0.5)
+
+for n_paths in sample_paths:
+    n_paths = int(n_paths)
+    all_values = 0
+
+    for _ in tqdm(range(n_paths)):
+        payoff = np.maximum(euler_option_valuation() - stock_price, 0)
+        option_value = np.exp(-r * t) * payoff
+        all_values += option_value
+
+    average_value = all_values / n_paths
+    option_values.append(average_value)
+
+plt.plot(sample_paths, option_values, 'r-')
+plt.xlim(min(sample_paths), max(sample_paths))
+plt.xscale('log')
+plt.xlabel("# Sample Paths")
+plt.ylabel("Option Value")
+plt.show()
