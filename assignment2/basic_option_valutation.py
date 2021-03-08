c410eb25e4cb3fc19084cdf696021a6f8f3aca18
Daan Moll <daanmoll@hotmail.com>
2021-03-08 11:27:13 +0100

    idk

--- assignment2/basic_option_valutation.py
+++ assignment2/basic_option_valutation.py
@@ -28,23 +28,30 @@ def euler_option_valuation(plot=False):
 
 
 option_values = []
-sample_trajectories = 10 ** np.arange(2, 7, 0.5)
+sample_trajectories = 4 ** np.arange(2, 7, 0.5)
+print(sample_trajectories)
 
 for n_trajectories in sample_trajectories:
     n_trajectories = int(n_trajectories)
-    all_values = 0
+    
 
-    for _ in tqdm(range(n_trajectories)):
-        payoff = np.maximum(euler_option_valuation() - strike_price, 0)
-        option_value = np.exp(-r * t) * payoff
-        all_values += option_value
+    for run in range(100):
+        all_values = 0
+        
+        for _ in tqdm(range(n_trajectories)):
+            payoff = np.maximum(euler_option_valuation() - strike_price, 0)
+            option_value = np.exp(-r * t) * payoff
+            all_values += option_value
 
-    average_value = all_values / n_trajectories
-    option_values.append(average_value)
+        average_value = all_values / n_trajectories
+        option_values.append(average_value)
 
-plt.plot(sample_trajectories, option_values, 'r-')
-plt.xlim(min(sample_trajectories), max(sample_trajectories))
-plt.xscale('log')
-plt.xlabel("# Sample trajectories")
-plt.ylabel("Option Value")
-plt.show()
+print(len(option_values))
+print(option_values)
+
+# plt.plot(sample_trajectories, option_values, 'r-')
+# plt.xlim(min(sample_trajectories), max(sample_trajectories))
+# plt.xscale('log')
+# plt.xlabel("# Sample trajectories")
+# plt.ylabel("Option Value")
+# plt.show()
